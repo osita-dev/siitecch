6300b5bff007c329a9b38e5f99c9de05849151ba
Christopher osita <70627592+osita-dev@users.noreply.github.com>
2025-11-25 11:39:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: 'https://siitecch-learn.vercel.app',
+  origin: 'http://localhost:5173',
   credentials: true,
 }));
 app.use(express.json());
